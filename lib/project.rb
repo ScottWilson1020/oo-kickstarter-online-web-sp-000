e82e4b3feba3e955de2050cc0557d6ee751a096d
Scott Wilson <scottwilson1020@gmail.com>
2020-08-18 05:56:58 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,13 @@
-class Project 
-  
-  def initialized 
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
     @backers = []
-  end 
+  end
   
-end 
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
